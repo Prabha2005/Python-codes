3d1418364f27c07eb03d4182fc95e03360da76cc
Lakshmi Prabha V <lakshmiprabhavl2005@gmail.com>
2026-03-16 17:48:27 +0530

    Removed folders in Python-branch

--- Patterns/RAT_mirror_2.py
+++
@@ -1,9 +0,0 @@
-# Right Angle Triangle Mirror Pattern 
-
-n = int(input("Enter the number of rows: "))
-for i in range(1, n + 1):
-    spaces = 2 * (n - i)
-    stars = i
-    rows = spaces * " " + "* " * stars
-
-    print(rows)
